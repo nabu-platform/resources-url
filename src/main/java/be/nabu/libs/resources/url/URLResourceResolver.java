489b1e52f7662eba2e939d7f7d0630a224ad92ea
alex <alex@nabu.be>
2016-02-04 12:08:54 +0100

    Reuse centralized default schemes

--- src/main/java/be/nabu/libs/resources/url/URLResourceResolver.java
+++ src/main/java/be/nabu/libs/resources/url/URLResourceResolver.java
@@ -3,24 +3,22 @@ package be.nabu.libs.resources.url;
 import java.io.IOException;
 import java.net.URI;
 import java.security.Principal;
-import java.util.Arrays;
 import java.util.List;
 
 import be.nabu.libs.resources.api.Resource;
 import be.nabu.libs.resources.api.ResourceResolver;
+import be.nabu.libs.resources.internal.VFSURLStreamHandlerFactory;
 
 public class URLResourceResolver implements ResourceResolver {
 
-	private static List<String> defaultSchemes = Arrays.asList(new String [] { "http", "https", "ftp", "jar" });
-	
 	@Override
 	public List<String> getDefaultSchemes() {
-		return defaultSchemes;
+		return VFSURLStreamHandlerFactory.defaultSchemes;
 	}
 
 	@Override
 	public Resource getResource(URI uri, Principal principal) throws IOException {
-		if (defaultSchemes.contains(uri.getScheme())) {
+		if (VFSURLStreamHandlerFactory.defaultSchemes.contains(uri.getScheme())) {
 			try {
 				uri.toURL().openStream().close();
 				return new URLReadableResource(null, uri.toURL());
